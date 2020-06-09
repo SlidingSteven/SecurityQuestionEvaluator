579fa6cb94dab979c5fb7d61aa1a39b572b3ce8a
slidingsteven <steven.tuckerba@ou.edu>
2020-06-08 23:29:17 -0500

    Adding instructions for making an API in app.py

--- app.py
+++ app.py
@@ -78,15 +78,28 @@ def fullSearch():
                 return render_template('SSQ Checker/FullSearchDemo.html', PeopleFound = "No Potential Information Found", flag = True)
         except ValueError:
             return render_template('SSQ Checker/FullSearchDemo.html', Error = "No Potential Information Found")
+    # else = GET request
     else:
+        # check for query params (section following the ? in the url below)
+        # https://sqs-checker.herokuapp.com/Full-Public-Info-Search?first_name=Bobby&last_name=Sue&zipcode=12345
+        # by checking for these particular params, I am forcing the user to also use those particular params 
+        # In other words, this is how the params  are set to 'first_name', 'last_name' and 'zipcode'
         if 'first_name' in request.args and 'last_name' in request.args and 'zipcode' in request.args:
+            # if the params exist then try to do a search with their contents
             try: 
+                # get the params from the url
                 first_name = request.args['first_name']
                 last_name = request.args['last_name']
                 zipcode = request.args['zipcode']
+
+                # perform the same search as we did with the post request 
                 PeopleFound = publicInfoSearch.publicInformation(first_name, last_name, zipcode)
                 i=0
+
+                # declare a list to put person results in 
                 data_list = []
+
+                # for each person in the list that was found, add their person info
                 for person in PeopleFound:
                     curr_person = {
                         "name": PeopleFound[i].get_name(),
@@ -95,15 +108,23 @@ def fullSearch():
                         "aliases": PeopleFound[i].get_aliases(),
                         "family": PeopleFound[i].get_family()
                     }
+
+                    #add the person to the list
                     data_list.append(curr_person)
                     i+=1
+                
+                # return the list as a json object
                 return jsonify(data_list)
+
+            # if 1- the contents are formatted incorrectly, or 2- the contents return no results then return an error message
             except:
                 return """Error: A field is missing or no data was returned.  
                 See below for formatting-\n
                 https://sqs-checker.herokuapp.com/Full-Public-Info-Search?first_name=Bobby&last_name=Sue&zipcode=12345
                 
                 If you think you have it properly formatted you can try searching your name on fastpeoplesearch.com which is where I scrape from."""
+        
+        # if there are no query params then just return the html for the entry
         return render_template('SSQ Checker/FullSearchDemo.html')
 
 #Mother's maiden name demo
@@ -140,8 +161,12 @@ def secureAnswers():
         component3 = request.form['component3']
         component4 = request.form['component4']
 
-
-
+        #
+        #
+        #   COPY SECTION FOR API
+        #
+        #
+        # TODO FOR API: COPY FROM HERE
         listOfRecommendations = []
         numComponents = 0
         if component0:
@@ -181,10 +206,21 @@ def secureAnswers():
             for i in list(perm): 
                 betterFormattedList.append(i[0]+"--"+i[1]+"--"+i[2]+"--"+i[3]+"--"+i[4])
             print(betterFormattedList)
+        # TODO FOR API: TO HERE
+        #
+        #
+        #   COPY SECTION FOR API
+        #
+        #
+
         return render_template('SSQ Checker/SecureInput.html', betterFormattedList = betterFormattedList)
 
         
     else:
+        # check if query params exist (If it were me I'd check for "component1, component2... so on, just like in the post method above.")
+            # if the query params exist then run the code from line 170-208 (I have comments around the code), return the jsonify-ed list
+            # else return error message
+ 
         return render_template('SSQ Checker/SecureInput.html')#, ruling = "SAFE")
 
 
